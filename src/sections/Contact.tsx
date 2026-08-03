ca9b399754fdbd785b25a4d317e4bb8767cf3436
TareqJudehGithub <tareq.joudeh@gmail.com>
2026-08-03 03:19:45 +0300

    Removed . from Contact component header for better visuals.

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -102,7 +102,7 @@ export const Contact = () => {
 					<h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground">
 						Let's build{" "}
 						<span className="font-serif italic font-normal text-white">
-							something great.
+							something great
 						</span>
 					</h2>
 					<p className="text-muted-foreground animate-fade-in animation-delay-200">
